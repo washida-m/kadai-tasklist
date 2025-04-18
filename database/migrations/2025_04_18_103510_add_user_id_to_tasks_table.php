abbedf52b7b920bc9ca8cddea2ba18011defbf22
mami.washida <washida_m@so-labo.co.jp>
2025-04-18 11:08:55 +0900

    laravel breeze daisyui

---
+++ database/migrations/2025_04_18_103510_add_user_id_to_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+
+            // 外部キー制約
+            $table->foreign('user_id')->references('id')->on('users');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+            $table->dropForeign('tasks_user_id_foreign');
+            // $table->dropForeign(['user_id']); 上記の省略形でも使用可能
+        });
+    }
+};
